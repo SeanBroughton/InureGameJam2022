51d4165acbf524e461c1340ca21867a5fd7e67c4
Sean Broughton <seanbroughton03@gmail.com>
2022-05-24 12:44:19 -0400

    Created more ground to run on in the Level 1 Scene, and created a FlipSprite Method to make sure the player is facing the correct direction when moving.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,13 +13,15 @@ public class PlayerMovement : MonoBehaviour
     
     void Start()
     {
+        // Gets access to the players physics system
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    
+    //Runs all of our methods
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     //creates number values to create player movement ex: (1,0) (0,1)
@@ -33,5 +35,16 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+    }
+
+    //makes the player flip facing direction when moving
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed)
+        {
+             transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+       
     }
 }
