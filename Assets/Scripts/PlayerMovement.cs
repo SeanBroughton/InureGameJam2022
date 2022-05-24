b740fef93841f7b99463be077bc454d03eca0657
Sean Broughton <seanbroughton03@gmail.com>
2022-05-24 12:31:52 -0400

    Added a Run Method, and created player movement on the X-Axis

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,22 +8,30 @@ public class PlayerMovement : MonoBehaviour
    // Added UnityEngine.InputSystem to use player controls
 
     Vector2 moveInput;
-
+    Rigidbody2D myRigidbody;
+    [SerializeField] float runSpeed = 10f;
+    
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
     {
-        
+        Run();
     }
 
     //creates number values to create player movement ex: (1,0) (0,1)
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    //gives the player movement and speed on the x axis
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
     }
 }
