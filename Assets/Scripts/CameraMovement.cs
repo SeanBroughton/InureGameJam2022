14c3b08247157043043d28273c8d162b6ee1e7bb
Luka Trikha <lukadtrikha@gmail.com>
2022-05-24 16:30:23 -0400

    Add camera tracking on player throughout the level.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform followTransform;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+    }
+}
