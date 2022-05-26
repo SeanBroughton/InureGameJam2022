0c5d3d0bce062807a8c58aea428c7d56e1156bae
Sean Broughton <seanbroughton03@gmail.com>
2022-05-26 14:26:28 -0400

    Created a new Boxcollider2D for the player, added code to stop the player from wall jumping, fixed bug where code didnt work, and adjusted level 1 for testing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,8 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
-    CapsuleCollider2D myCapsuleCollider;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
     float gravityScaleAtStart;
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 5f;
@@ -21,7 +22,8 @@ public class PlayerMovement : MonoBehaviour
         //gets access to the players components in unity to edit in the code script
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
         gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
@@ -44,13 +46,15 @@ public class PlayerMovement : MonoBehaviour
     {
 
         //allows the player to jump only when they are touching the Ground
-        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
+        if(!myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
+       
 
         if(value.isPressed)
         {
             myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
         }
 
+        if(myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
       
     }
 
@@ -65,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
         myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
 
         //stops the run animation from playing while on a ladder
-        if(myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
+        if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
         {
             myAnimator.SetBool("isRunning", false);
             return;
@@ -87,7 +91,7 @@ public class PlayerMovement : MonoBehaviour
     void ClimbLadder()
     {
         //stops the player from climbing infinitely by locking climbing to the ladder sprite
-        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
         {
             myRigidbody.gravityScale = gravityScaleAtStart;
             myAnimator.SetBool("isClimbing", false);
