acb34d7eee3a14b1568cac3f8b8406d325ff20f8
Sean Broughton <seanbroughton03@gmail.com>
2022-05-28 12:15:41 -0400

    Added a UI to the game, added a new font type, created a lives & score tracker for the UI, adjusted the coin script to stop the ability to pick up coins twice.

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 1;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] int score = 0;
     
     //creates a game manager object that tracks levels and lives
     void Awake()
@@ -22,6 +26,14 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        //turns the player lives into text on screen
+        livesText.text = playerLives.ToString();
+        //displays the score on the screen
+        scoreText.text = score.ToString();
+    }
+
     //resets the game when player runs out of lives
     public void ProcessPlayerDeath()
     {
@@ -35,12 +47,22 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    //add points to the score counter
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+
     //reduces the amount of lives a player had and resets the level
     void TakeLife()
     {
        playerLives--;
        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
        SceneManager.LoadScene(currentSceneIndex);
+
+        //properly updates player lives text to the screen
+       livesText.text = playerLives.ToString();
     }
 
     //resets the game and refreshes the game manager
